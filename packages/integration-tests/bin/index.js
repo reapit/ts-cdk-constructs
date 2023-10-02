1adfbb6ac3f4eb0addf76e62bdf116a9ca755bda
Josh Balfour <josh@joshbalfour.co.uk>
2023-10-02 16:06:52 +0100

    fixes

--- packages/integration-tests/bin/index.js
+++ packages/integration-tests/bin/index.js
@@ -3,12 +3,17 @@ const { cli } = require('@aws-cdk/integ-runner')
 
 const isRoot = process.argv.slice(2).includes('--root')
 
-cli([
-  '--directory',
-  isRoot ? 'packages' : '.',
-  '--parallel-regions',
-  'eu-central-1',
-  '--language',
-  'typescript',
-  '--update-on-failed',
-])
+try {
+  cli([
+    '--directory',
+    isRoot ? 'packages' : '.',
+    '--parallel-regions',
+    'eu-central-1',
+    '--language',
+    'typescript',
+    '--update-on-failed',
+  ])
+} catch (e) {
+  console.error(e)
+  process.exit(1)
+}
