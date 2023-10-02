213a221d3c8c7e9c7cf0b7dd6cbcdc88bc5ea66f
Josh Balfour <josh@joshbalfour.co.uk>
2023-10-02 15:54:52 +0100

    hopefully fix eslint

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -18,14 +18,7 @@ module.exports = {
     project: './packages/*/tsconfig.json',
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  ignorePatterns: [
-    'node_modules/',
-    '**/node_modules/',
-    '**/dist',
-    'tsconfig.json',
-    '.eslintrc.js',
-    '*.snapshot/**/*.*',
-  ],
+  ignorePatterns: ['node_modules/', '**/node_modules/', '**/dist', 'tsconfig.json', '.eslintrc.js', '*.snapshot/'],
   rules: {
     quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
     semi: ['error', 'never'],
