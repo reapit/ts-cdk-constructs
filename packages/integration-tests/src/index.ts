96e0b719915e3246c1953d198aae90ed03b4b513
Josh Balfour <josh@joshbalfour.co.uk>
2023-10-03 17:09:17 +0100

    add env to AssertionStack

--- packages/integration-tests/src/index.ts
+++ packages/integration-tests/src/index.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib'
 // import { Aspects } from 'aws-cdk-lib'
 // import { AwsSolutionsChecks } from 'cdk-nag'
 export * from '@aws-cdk/integ-tests-alpha'
+import * as integ from '@aws-cdk/integ-tests-alpha'
 
 export class App extends cdk.App {
   constructor() {
@@ -18,6 +19,15 @@ export class Stack extends cdk.Stack {
         account: process.env.AWS_ACCOUNT,
         region: process.env.INTEG_REGION,
       },
+    })
+  }
+}
+
+export class IntegTest extends integ.IntegTest {
+  constructor(app: App, id: string, props: integ.IntegTestProps) {
+    super(app, id, {
+      ...props,
+      assertionStack: new Stack(app, id + 'Assertions'),
     })
   }
 }
