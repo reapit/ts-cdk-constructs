086b3b55923ced0791700eba5fafcd9f2cf7e183
Josh Balfour <josh@joshbalfour.co.uk>
2023-10-03 10:23:02 +0100

    add cloudfront-invalidation test

---
+++ packages/cloudfront-invalidation/integ-tests/integ.cloudfront-invalidation.ts
@@ -0,0 +1,57 @@
+import { IntegTest, ExpectedResult, App } from '@reapit-cdk/integration-tests'
+import { Stack } from 'aws-cdk-lib'
+import { randomUUID } from 'crypto'
+
+import { CloudfrontInvalidation } from '../dist'
+import { Distribution, PriceClass } from 'aws-cdk-lib/aws-cloudfront'
+import { HttpOrigin } from 'aws-cdk-lib/aws-cloudfront-origins'
+
+const app = new App()
+
+const stack = new Stack(app, 'cloudfront-invalidation-stack')
+const distribution = new Distribution(stack, 'distribution', {
+  defaultBehavior: {
+    origin: new HttpOrigin('example.org'),
+  },
+  priceClass: PriceClass.PRICE_CLASS_100,
+})
+
+const uuid = randomUUID()
+const items = ['/index.html', `/${uuid}`]
+
+new CloudfrontInvalidation(stack, 'invalidation', {
+  distribution,
+  items,
+  invalidateOnCreation: true,
+})
+const integ = new IntegTest(app, 'CloudfrontInvalidationTest', {
+  testCases: [stack],
+  cdkCommandOptions: {
+    destroy: {
+      args: {
+        force: true,
+      },
+    },
+  },
+  regions: [stack.region],
+})
+
+const assertion = integ.assertions
+  .awsApiCall('Cloudfront', 'ListInvalidations', {
+    distributionId: distribution.distributionId,
+  })
+  .expect(
+    ExpectedResult.objectLike({
+      InvalidationList: [
+        {
+          Items: items,
+        },
+      ],
+    }),
+  )
+
+assertion.provider.addToRolePolicy({
+  Effect: 'Allow',
+  Action: ['cloudfront:ListInvalidations'],
+  Resource: ['*'],
+})
