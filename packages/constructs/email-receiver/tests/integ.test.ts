31eaac60f12048ac491199a36232ea3de5613f9c
Josh Balfour <josh@joshbalfour.co.uk>
2023-10-11 12:07:34 +0100

    replicated-key new integration test

--- packages/constructs/email-receiver/tests/integ.test.ts
+++ packages/constructs/email-receiver/tests/integ.test.ts
@@ -9,7 +9,7 @@ describe('email-receiver integration', () => {
     stackName: 'email-receiver-test-stack',
   })
 
-  void integ.it('should output the active receiptRuleSetName', async () => {
+  void integ.it('should should receive the email and put it in the dynamodb table', async () => {
     const { recipient } = await sendTestEmail()
     const email = await waitForTestEmail(integ.outputs.output, recipient)
     expect(email).toBeDefined()
---
+++ packages/constructs/replicated-key/tests/integ.stack.ts
@@ -0,0 +1,15 @@
+import { App, Stack } from '@reapit-cdk/integration-tests'
+import { ReplicatedKey } from '../dist'
+import { CfnOutput } from 'aws-cdk-lib'
+
+const app = new App()
+
+const stack = new Stack(app, 'replicated-key-test-stack')
+
+const key = new ReplicatedKey(stack, 'replicated-key', {
+  replicaRegions: ['eu-west-1', 'eu-west-2'],
+})
+
+new CfnOutput(stack, 'output', {
+  value: key.getRegionalKey('ca-central-1').keyId,
+})
