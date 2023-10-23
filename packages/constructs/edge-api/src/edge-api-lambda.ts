a2e0010251c88cacfcd75c2e1780446101051d7c
Josh Balfour <josh@joshbalfour.co.uk>
2023-10-23 17:42:40 +0100

    override addEnvironment

--- packages/constructs/edge-api/src/edge-api-lambda.ts
+++ packages/constructs/edge-api/src/edge-api-lambda.ts
@@ -3,10 +3,17 @@ import { Construct } from 'constructs'
 import { EdgeFunctionProps } from 'aws-cdk-lib/aws-cloudfront/lib/experimental'
 
 export class EdgeAPILambda extends Function {
-  edgeEnvironment?: FunctionProps['environment']
+  edgeEnvironment: FunctionProps['environment']
   constructor(scope: Construct, id: string, props: EdgeFunctionProps & { environment?: FunctionProps['environment'] }) {
     const { environment, ...rest } = props
     super(scope, id, rest)
     this.edgeEnvironment = environment
+  }
+  addEnvironment(key: string, value: any): this {
+    if (!this.edgeEnvironment) {
+      this.edgeEnvironment = {}
+    }
+    this.edgeEnvironment[key] = value
+    return this
   }
 }
