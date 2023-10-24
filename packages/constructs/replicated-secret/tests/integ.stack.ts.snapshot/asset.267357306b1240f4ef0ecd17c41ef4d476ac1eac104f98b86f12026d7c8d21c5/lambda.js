c40e7cbf4ef35cd32a990994960b70905afdfc13
joshbalfour <joshbalfour@users.noreply.github.com>
2023-10-24 11:56:15 +0000

    Apply integration test snapshot changes

--- packages/constructs/replicated-secret/tests/integ.stack.ts.snapshot/asset.267357306b1240f4ef0ecd17c41ef4d476ac1eac104f98b86f12026d7c8d21c5/lambda.js
+++ packages/constructs/replicated-secret/tests/integ.stack.ts.snapshot/asset.267357306b1240f4ef0ecd17c41ef4d476ac1eac104f98b86f12026d7c8d21c5/lambda.js
@@ -24,38 +24,6 @@ __export(lambda_exports, {
 });
 module.exports = __toCommonJS(lambda_exports);
 
-// ../../modules/common/src/regions.ts
-var awsRegions = [
-  "us-east-1",
-  "us-east-2",
-  "us-west-1",
-  "us-west-2",
-  "us-gov-west-1",
-  "us-gov-east-1",
-  "ca-central-1",
-  "eu-north-1",
-  "eu-west-1",
-  "eu-west-2",
-  "eu-west-3",
-  "eu-central-1",
-  "eu-south-1",
-  "af-south-1",
-  "ap-northeast-1",
-  "ap-northeast-2",
-  "ap-northeast-3",
-  "ap-southeast-1",
-  "ap-southeast-2",
-  "ap-southeast-3",
-  "ap-east-1",
-  "ap-south-1",
-  "ap-south-2",
-  "sa-east-1",
-  "me-south-1",
-  "cn-north-1",
-  "cn-northwest-1"
-];
-var stringIsAWSRegion = (str) => awsRegions.includes(str);
-
 // src/lambda/ensure-replication.ts
 var import_client_kms2 = require("@aws-sdk/client-kms");
 
@@ -230,7 +198,7 @@ var validateKeyMetadata = (keyMetadata) => {
       throw new Error("given key is not multiregion primary key");
     }
     if (ReplicaKeys) {
-      const existingRegions = ReplicaKeys.map((key) => key.Region).filter(strIsDefined).filter(stringIsAWSRegion);
+      const existingRegions = ReplicaKeys.map((key) => key.Region).filter(strIsDefined);
       return existingRegions;
     }
   }
