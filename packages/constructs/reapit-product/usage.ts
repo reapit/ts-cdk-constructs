17a02135c83bb3ba5b7bd700f93e0affa6bcaf75
Josh Balfour <josh@joshbalfour.co.uk>
2023-11-15 15:53:55 +0000

    update reapit-product usage

--- packages/constructs/reapit-product/usage.ts
+++ packages/constructs/reapit-product/usage.ts
@@ -1,11 +1,17 @@
 import { CfnOutput, Stack, App } from 'aws-cdk-lib'
 import { ReapitProductProvider } from '@reapit-cdk/reapit-product'
+import { RestApi } from 'aws-cdk-lib/aws-apigateway'
 
 const app = new App()
 const stack = new Stack(app, 'stack-name')
 
+const orgsApiGwId = '' // imported from somewhere or hard coded
+
+const organisationsServiceApiGateway = RestApi.fromRestApiId(stack, 'orgs-api-gw', orgsApiGwId)
+
 const productProvider = new ReapitProductProvider(stack, 'product-provider', {
-  organisationsServiceUrl: '',
+  organisationsServiceApiGateway,
+  stageName: 'api',
 })
 
 const product = productProvider.createProduct(stack, 'product', {
