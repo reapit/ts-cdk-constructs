538dbc62b75ef540347be6bce9afeaa54d2939d1
Josh Balfour <josh@joshbalfour.co.uk>
2024-02-23 12:53:45 +0000

    maybe fix eslint

--- packages/constructs/cloudfront-invalidation/.eslintrc.js
+++ packages/constructs/cloudfront-invalidation/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: ["@reapit-cdk/eslint-config"],
+  parserOptions: {
+    ecmaVersion: 'ESNext',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
 }
--- packages/tools/integration-tests/.eslintrc.js
+++ packages/tools/integration-tests/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: ["@reapit-cdk/eslint-config"],
+  parserOptions: {
+    ecmaVersion: 'ESNext',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  parserOptions: {
+    ecmaVersion: 'ESNext',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
 }
