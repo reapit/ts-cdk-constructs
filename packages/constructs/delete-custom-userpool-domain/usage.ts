797245f5128bdbe60bbfae243be8716d634c9fc8
Craig Duggan <cduggan@reapit.com>
2024-05-13 08:53:03 +0100

    docs: update usage to reference package rather than local file

--- packages/constructs/delete-custom-userpool-domain/usage.ts
+++ packages/constructs/delete-custom-userpool-domain/usage.ts
@@ -1,6 +1,6 @@
 import { Stack, App } from 'aws-cdk-lib'
 import { UserPool } from 'aws-cdk-lib/aws-cognito'
-import { DeleteCustomUserPoolDomain } from './src/delete-custom-userpool-domain'
+import { DeleteCustomUserPoolDomain } from '@reapit-cdk/delete-custom-userpool-domain'
 
 const app = new App()
 const stack = new Stack(app, 'stack-name')
